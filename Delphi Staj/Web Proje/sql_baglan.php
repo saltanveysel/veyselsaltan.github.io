38779c18091396ec14b39f4062469f42add2cc7e
Veysel Saltan <veyselsaltan.github.com>
2020-05-05 17:06:29 +0300

    Removed DS

--- Delphi Staj/Web Proje/sql_baglan.php
+++
@@ -1,9 +0,0 @@
-﻿<?php
-   @$baglanti = mysqli_connect("localhost", "root", "","bilgi_yarismasi");
-   if ( !$baglanti )
-	exit( "Bağlantı sağlanamadı." );
-   mysqli_select_db( $baglanti,"telefon_defteri" );
-   mysqli_query($baglanti,"SET NAMES UTF8");
-   
-
-?>
